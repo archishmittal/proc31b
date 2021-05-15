3c2ef45fa93f1ae3bb19d2a476bbd649c397576f
archishmittal <75306301+archishmittal@users.noreply.github.com>
2021-05-15 14:00:38 +0530

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,35 @@
+class Particle {
+    constructor(x, y, r) {
+        var options = {
+            restitution: 1,
+            friction: 0,
+            isStatic:true
+        }
+        this.r = 10;
+        this.body = Bodies.circle(x, y, this.r, options);
+      
+        
+        World.add(world, this.body);
+    }
+    display() {
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        if(frameCount%60===0)
+        {
+          particles.push(new Particle(random(width/2-10, width/2+10),10 ,10));
+        }
+        
+        push();
+        translate(pos.x, pos.y);
+        this.color=color(random(0,255), random(0,255), random(0,255));
+        rotate(angle);
+        imageMode(CENTER);
+        noStroke();
+        fill("white");
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.r,this.r);
+        pop();
+    }
+
+};
